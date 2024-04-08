b56e984be13a34245a4b5af277e6eb816003caa9
Simon <sawsimonelinn@gmail.com>
2024-04-07 22:09:27 -0700

    new stage-3 commit

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,15 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+const profileEditModal = document.querySelector("#profile-edit-modal");
+
+const profileEditButton = document.querySelector("#profile__edit-button");
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+
+const modalCloseButton = document.querySelector("#modal__close-button");
+modalCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_opened");
+});
